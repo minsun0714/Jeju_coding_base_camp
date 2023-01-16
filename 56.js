66e14d96e15fff80eca19f246c49229dc1182655
minsun0714 <woooooooow22@gmail.com>
2023-01-16 09:46:41 +0900

    ✨ 객체의 함수 응용 (korea와 면적이 최소 차이나는 국가 구하기 )

---
+++ 56.js
@@ -0,0 +1,30 @@
+const nationWidth = {
+  korea: 220877,
+  Russia: 17098242,
+  China: 9596961,
+  France: 543965,
+  Japan: 377915,
+  England: 242900,
+};
+
+const w = nationWidth["korea"];
+delete nationWidth["korea"];
+// console.log(nationWidth);
+
+const entry = Object.entries(nationWidth);
+// console.log(entry);
+const values = Object.values(nationWidth);
+// console.log(values);
+
+//
+let gap = Math.max.apply(null, values);
+// console.log(gap);
+let item = [];
+
+for (let i in entry) {
+  if (gap > Math.abs(entry[i][1] - w)) {
+    gap = Math.abs(entry[i][1] - w);
+    item = entry[i];
+  }
+}
+console.log(item[0], item[1] - w);
